f994b59f8d4947fb183864db066369154b9e093e
Shane Whitehead <shane.whitehead@beamcommunications.com>
2017-06-28 17:40:06 +1000

    Xcode 9b2

--- makerelease.sh
+++ makerelease.sh
@@ -1,2 +1,6 @@
-carthage build --no-skip-current
-carthage archive CocoaAsyncSocket
+sudo xcode-select -s /Applications/Xcode-beta.app/Contents/Developer
+xcodebuild -version
+time carthage build --no-skip-current
+time carthage archive CocoaAsyncSocket
+sudo xcode-select -s /Applications/Xcode.app/Contents/Developer
+xcodebuild -version
