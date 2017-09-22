7891948f5398503f5b380350e836bf845b7a245e
Shane Whitehead <obishiro@gmail.com>
2017-09-22 12:09:08 +1000

    Add build scripts

---
+++ makerelease.sh
@@ -0,0 +1,8 @@
+sudo xcode-select -s /Applications/Xcode-beta.app/Contents/Developer
+xcodebuild -version
+rm *.zip
+rm -rf Carthage
+time carthage build --no-skip-current
+time carthage archive CocoaAsyncSocket
+sudo xcode-select -s /Applications/Xcode.app/Contents/Developer
+xcodebuild -version
